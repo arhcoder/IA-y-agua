97e3fe8c2712fae60c3d845344f4bca2c13e725f
Alejandro Ramos <arhcoder@gmail.com>
2023-04-10 15:09:00 -0600

    Cambios mínimos para el cálculo del agua
    No más informaćión...

--- Algorithms/Caculo_Agua.py
+++ Algorithms/Caculo_Agua.py
@@ -1,10 +1,10 @@
 
-def Caculo_Agua_Necesaria_para_Regar(ETc,Eto,Kc):
+def Caculo_Agua_Necesaria_para_Regar(Eto,Kc):
     return Eto * Kc
     
 
 
-def main():
+if __name__ == "__main__":
     
     Eto=0.0
     Kc=0.0
@@ -12,8 +12,4 @@ def main():
     Eto=float(input("Eto: "))
     Kc=float(input("Kc: "))
 
-    print("La cantidad de agua necesaria para regar es: ",Caculo_Agua_Necesaria_para_Regar(ETc,Eto,Kc))
-    
-
-
-
+    print("La cantidad de agua necesaria para regar es: ",Caculo_Agua_Necesaria_para_Regar(Eto, Kc))
